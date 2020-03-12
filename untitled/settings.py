5d2e2e6de4d45bccc5a38e4927e53e49a710cb0e
David <nundavid99@gmail.com>
2020-03-12 19:27:38 -0300

    Cambios para

--- untitled/settings.py
+++ untitled/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'PaoApp.apps.PaoappConfig',
 
 ]
@@ -121,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
